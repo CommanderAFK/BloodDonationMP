ad8206d8634593e56691cd073f6dd8811f0ec916
MSI\user <commanderafk@gmail.com>
2023-02-17 04:33:32 +0800

    First commit

--- app/src/main/java/com/example/blooddonationapplication/homePage.java
+++ app/src/main/java/com/example/blooddonationapplication/homePage.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.TextView;
 
@@ -55,4 +57,6 @@ public class homePage extends AppCompatActivity {
         Intent intent = new Intent(this,ThingToDo.class);
         startActivity(intent);
     }
+
+
 }
